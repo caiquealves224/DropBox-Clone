a10d0efb32659f9953f09aa62e95d57be0b261f2
caiquealves224 <caiquealves224@gmail.com>
2019-04-10 20:22:47 -0300

    adicionando multer

--- backend/src/controllers/BoxController.js
+++ backend/src/controllers/BoxController.js
@@ -3,7 +3,7 @@ const Box = require("../models/box");
 class BoxController {
     async store( req, res ) {
 
-        const box = await Box.create({ title : "RocketSeat" }) ;
+        const box = await Box.create(req.body) ;
 
         return res.json(box)
     }
